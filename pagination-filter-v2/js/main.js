ab6e28504dfa30f85a175999797a8333b0b40c90
Amber Febbraro <amberfebbraro@quickenloans.com>
2016-11-30 13:37:57 -0500

    add instructions

--- pagination-filter-v2/js/main.js
+++ pagination-filter-v2/js/main.js
@@ -1,3 +1,12 @@
+/*
+    Project Instructions
+    When the page loads, your program should hide all but the first 10 students in the list.
+    Look at the HTML in the example-meets.html on lines 119-137 -- this is an example of the markup you'll need to add dynamically to the index.html page to create pagination links.
+    Since only 10 students should be shown at a time, your programming needs to calculate the number of pages needed and add the appropriate number of links to the bottom of the page.
+    When a user clicks on “2” in the pagination, students 11 through 20 are shown. When a user clicks “3”, students 21 through 30 are shown. And so on. When “6” is clicked 51 through 55 should be shown.
+    Your program should work for any number of students. There are 54 students in index.html, but you can test your code by adding the JavaScript file your write to the other lists of students we’ve provided in the student-list-examples folder.
+*/
+
 // get the number of students in the list by doing a query on the studentlist ul for li elements, type = num
 var students = document.getElementById('js-student-list').querySelectorAll('li'),
     paginationDiv = document.getElementById('js-pagination'),
