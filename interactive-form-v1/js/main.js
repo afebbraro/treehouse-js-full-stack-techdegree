d94755f42689bdaacbacb5f069a63c4721d67935
Amber Febbraro <amberfebbraro@quickenloans.com>
2016-12-08 16:51:27 -0500

    first pass at this project

--- interactive-form-v1/js/main.js
+++ interactive-form-v1/js/main.js
@@ -1,5 +1,3 @@
-console.log('js works!');
-
 /*
 Project Instructions
 Set focus on the first text field
@@ -94,12 +92,86 @@ function shirtDesignColors() {
     });
 }
 
-// ”Register for Activities” section of the form:
-//     Some events are at the same time as others. If the user selects a workshop, don't allow selection of a workshop at the same date and time -- you should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
-//     When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled.
-//     As a user selects activities, a running total should display below the list of checkboxes. For example, if the user selects "Main Conference", then Total: $200 should appear. If they add 1 workshop, the total should change to Total: $300.
+var activityFieldset = document.getElementById('js-activities'),
+    totalPrice = 0,
+    totalMsg = document.createElement('p');
 
+// If the user selects a workshop, don't allow selection of a workshop at the same date and time
+function workshopCheckboxes() {
+    for (var i = 1; i < activityFieldset.children.length; i++) {
+        activityFieldset.children[i].querySelector('input[type=checkbox]').addEventListener('change', function() {
+            if (this.checked) {
+                if (this.name === 'js-frameworks') {
+                    activityFieldset.querySelector('input[name=express]').disabled = true;
+                } else if (this.name === 'express') {
+                    activityFieldset.querySelector('input[name=js-frameworks]').disabled = true;
+                } else if (this.name === 'js-libs') {
+                    activityFieldset.querySelector('input[name=node]').disabled = true;
+                } else if (this.name === 'node') {
+                    activityFieldset.querySelector('input[name=js-libs]').disabled = true;
+                }
+            } else if (this.checked === false && this.name === 'js-frameworks') {
+                activityFieldset.querySelector('input[name=express]').disabled = false;
+            } else if (this.checked === false && this.name === 'express') {
+                activityFieldset.querySelector('input[name=js-frameworks]').disabled = false;
+            } else if (this.checked === false && this.name === 'js-libs') {
+                activityFieldset.querySelector('input[name=node]').disabled = false;
+            } else if (this.checked === false && this.name === 'node') {
+                activityFieldset.querySelector('input[name=js-libs]').disabled = false;
+            } else {
+                activityFieldset.querySelector('input[name=node]').disabled = false;
+                activityFieldset.querySelector('input[name=js-libs]').disabled = false;
+                activityFieldset.querySelector('input[name=js-frameworks]').disabled = false;
+            }
+            runningTotal(this);
+        });
+    }
+}
 
+function runningTotal(thisCheckbox) {
+    if (thisCheckbox.checked) {
+        if (thisCheckbox.name === 'all') {
+            totalPrice += 200;
+        } else {
+            totalPrice += 100;
+        }
+    } else if (thisCheckbox.checked === false && thisCheckbox.name === 'all') {
+        totalPrice -= 200;
+    } else {
+        totalPrice -= 100;
+    }
+    totalMsg.innerHTML = 'Total: $' + totalPrice;
+    activityFieldset.appendChild(totalMsg);
+}
+
+function payment() {
+    // The "Credit Card" payment option should be selected by default,
+    document.getElementById('payment').value = 'credit card';
+    document.getElementById('js-paypal-info').classList.add('is-hidden');
+    document.getElementById('js-bitcoin-info').classList.add('is-hidden');
+
+    document.getElementById('payment').addEventListener('change', function() {
+        if (document.getElementById('payment').value === 'credit card') {
+            document.getElementById('js-paypal-info').classList.add('is-hidden');
+            document.getElementById('credit-card').classList.remove('is-hidden');
+            document.getElementById('js-bitcoin-info').classList.add('is-hidden');
+        } else if (document.getElementById('payment').value === 'paypal') {
+            console.log('paypal');
+            document.getElementById('js-paypal-info').classList.remove('is-hidden');
+            document.getElementById('js-bitcoin-info').classList.add('is-hidden');
+            document.getElementById('credit-card').classList.add('is-hidden');
+        } else if (document.getElementById('payment').value === 'bitcoin') {
+            console.log('bitcoin');
+            document.getElementById('js-paypal-info').classList.add('is-hidden');
+            document.getElementById('js-bitcoin-info').classList.remove('is-hidden');
+            document.getElementById('credit-card').classList.add('is-hidden');
+        } else {
+            document.getElementById('js-paypal-info').classList.add('is-hidden');
+            document.getElementById('js-bitcoin-info').classList.add('is-hidden');
+            document.getElementById('credit-card').classList.add('is-hidden');
+        }
+    })
+}
 
 window.onload = function() {
     // When the page loads, give focus to the first text field
@@ -108,4 +180,8 @@ window.onload = function() {
     revealOtherJobRoleField();
 
     shirtDesignColors();
+
+    workshopCheckboxes();
+
+    payment();
 };
